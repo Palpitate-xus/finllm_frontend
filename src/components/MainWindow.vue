a2bf4190ebf63113a1684c18ebf9794ac7d9599d
Palpitate <xus1758011080@outlook.com>
2024-02-19 13:27:36 +0800

    修改窗口布局

--- src/components/MainWindow.vue
+++ src/components/MainWindow.vue
@@ -1,35 +1,37 @@
 <template>
-    <el-container>
-      <el-aside width="30%">
-        <el-input
-          type="textarea"
-          placeholder="请输入内容"
-          v-model="message"
-        ></el-input>
-        <el-upload
-          action="https://jsonplaceholder.typicode.com/posts/"
-          on-preview="handlePreview"
-          on-remove="handleRemove"
-          before-remove="beforeRemove"
-          :file-list="fileList"
-        >
-          <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
-          <el-button style="margin-left: 10px;" size="small" type="success">上传到服务器</el-button>
-        </el-upload>
-      </el-aside>
-      <el-main>
-        <el-button @click="handleClick1">功能1</el-button>
-        <el-button @click="handleClick2">功能2</el-button>
-        <!-- 添加更多按钮 -->
-      </el-main>
-      <el-aside width="30%">
-        <el-input
-          type="textarea"
-          placeholder="输出内容"
-          v-model="output"
-          :readonly="true"
-        ></el-input>
-      </el-aside>
+    <el-container direction="vertical">
+      <el-row>
+        <el-col :span="8">
+          <el-input
+            type="textarea"
+            placeholder="请输入内容"
+            v-model="message"
+          ></el-input>
+          <el-upload
+            action="https://jsonplaceholder.typicode.com/posts/"
+            on-preview="handlePreview"
+            on-remove="handleRemove"
+            before-remove="beforeRemove"
+            :file-list="fileList"
+          >
+            <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+            <el-button style="margin-left: 10px;" size="small" type="success">上传到服务器</el-button>
+          </el-upload>
+        </el-col>
+        <el-col :span="8" class="button-column">
+          <el-button @click="handleClick1">功能1</el-button>
+          <el-button @click="handleClick2">功能2</el-button>
+          <!-- 添加更多按钮 -->
+        </el-col>
+        <el-col :span="8">
+          <el-input
+            type="textarea"
+            placeholder="输出内容"
+            v-model="output"
+            :readonly="true"
+          ></el-input>
+        </el-col>
+      </el-row>
     </el-container>
   </template>
   
@@ -51,7 +53,7 @@
         console.log('Remove uploaded file:', file, fileList);
       },
       beforeRemove(file, fileList) {
-        return this.$confirm(`确定移除 ${file.name}？`);
+        return this.$confirm(`确定移除 ${file.name, fileList}？`);
       },
       handleClick1() {
         // 实现功能1
@@ -64,8 +66,19 @@
   </script>
   
   <style scoped>
-  .el-container {
+  .el-row {
     height: 100vh;
+  }
+  
+  .button-column {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .button-column .el-button {
+    margin-bottom: 10px;
   }
   </style>
   
