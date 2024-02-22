c33fbe420bc51fa8aa83d6c568c0e08c0425156a
Palpitate <xus1758011080@outlook.com>
2024-02-22 11:00:57 +0800

    添加登陆页面

---
+++ src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+  name: 'LoginComponent',
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      loginRules: {
+        username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+        password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
+      },
+      loading: false,
+  }
+}
+}
+</script>
