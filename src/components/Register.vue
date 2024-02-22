b7d668857b0c62c78a0cc0942c53e8750e8906f2
Palpitate <xus1758011080@outlook.com>
2024-02-22 11:01:04 +0800

    添加注册页面

---
+++ src/components/Register.vue
@@ -0,0 +1,27 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: 'RegisterComponent',
+    data () {
+        return {
+            message: '',
+            output: '',
+            fileList: [],
+            methods: {
+                submitUpload() {
+                    console.log('submitUpload');
+                },
+                handlePreview(file) {
+                    console.log('File preview:', file);
+                },
+                handleRemove(file, fileList) {
+                    console.log('Remove uploaded file:', file, fileList);
+                }
+            }
+        }
+    }
+}
+</script>
