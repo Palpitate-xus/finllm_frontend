fd65d9c9fc970fb37e457809767b80d6a6723185
Palpitate <xus1758011080@outlook.com>
2024-02-22 10:59:46 +0800

    添加路由

---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import Router from 'vue-router'
+import Vue from 'vue'
+Vue.use(Router)
+
+import HelloWorld from '../components/HelloWorld.vue'
+import LoginComponent from '../components/Login.vue'
+import RegisterComponent from '../components/Register.vue'
+import MainWindow from '../components/MainWindow.vue'
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HelloWorld
+    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   component: About
+    // },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginComponent
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterComponent
+    },
+    {
+      path: '/qa',
+      name: 'qa',
+      component: MainWindow
+    }
+  ]
+})
