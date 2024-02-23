34cb3bbb95a49b58bf6459dad53a28dbb28b5d0c
Palpitate <xus1758011080@outlook.com>
2024-02-23 11:57:00 +0800

    add About Page

---
+++ src/components/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <el-row :gutter="20">
+        <h1>关于</h1>
+      </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'AboutComponent',
+    data() {
+        return {
+        }
+    },
+    methods: {
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
