ed000b574284b013b69ad8428f0d1e7a0be135fd
Palpitate <xus1758011080@outlook.com>
2024-02-23 11:57:07 +0800

    add Profile page

---
+++ src/components/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Profile</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProfileComponent',
+  data () {
+    return {
+      
+  }
+}
+}
+</script>
+
+<style>
+</style>
