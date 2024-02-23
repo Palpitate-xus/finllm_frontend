d74281e229a207014916b706415b276e94424b11
Palpitate <xus1758011080@outlook.com>
2024-02-23 14:17:21 +0800

    update register process

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,27 +1,95 @@
 <template>
-    <div></div>
+    <div class="register-container">
+      <el-card class="register-card">
+        <h3>注册</h3>
+        <el-form ref="registerForm" :model="registerForm" :rules="registerRules" label-width="80px">
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="registerForm.username" placeholder="请输入用户名"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱" prop="email">
+            <el-input v-model="registerForm.email" placeholder="请输入邮箱"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input type="password" v-model="registerForm.password" placeholder="请输入密码"></el-input>
+          </el-form-item>
+          <el-form-item label="确认密码" prop="confirmPassword">
+            <el-input type="password" v-model="registerForm.confirmPassword" placeholder="请再次输入密码"></el-input>
+          </el-form-item>
+          <el-form-item class="button-container">
+            <el-button type="primary" @click="register">注册</el-button>
+            <el-button @click="goToLogin">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </el-card>
+ </div>
 </template>
 
 <script>
 export default {
     name: 'RegisterComponent',
-    data () {
-        return {
-            message: '',
-            output: '',
-            fileList: [],
-            methods: {
-                submitUpload() {
-                    console.log('submitUpload');
-                },
-                handlePreview(file) {
-                    console.log('File preview:', file);
-                },
-                handleRemove(file, fileList) {
-                    console.log('Remove uploaded file:', file, fileList);
-                }
-            }
-        }
-    }
+    data() {
+      return {
+        registerForm: {
+          username: '',
+          email: '',
+          password: '',
+          confirmPassword: '',
+        },
+        registerRules: {
+          username: [{ required: true, message: '用户名不能为空', trigger: 'blur' }],
+          email: [
+            { required: true, message: '邮箱不能为空', trigger: 'blur' },
+            { type: 'email', message: '邮箱格式错误', trigger: 'blur' },
+          ],
+          password: [
+            { required: true, message: '密码不能为空', trigger: 'blur' },
+            { min: 6, message: '密码至少6位', trigger: 'blur' },
+          ],
+          confirmPassword: [
+            { required: true, message: '请再次输入密码', trigger: 'blur' },
+            { validator: this.validatePasswordConfirmation, trigger: 'blur' },
+          ],
+        },
+      };
+    },
+    methods: {
+      goToLogin() {
+        this.$router.push('/login');
+      },
+      register() {
+        this.$refs.registerForm.validate(valid => {
+          if (valid) {
+            this.$store.dispatch('register', this.registerForm).then(() => {
+              this.$router.push('/');
+            });
+          }
+        });
+      },
+    //   validatePasswordConfirmation(rule, value, callback) {
+    //     if (value !== this.registerForm.password) {
+    //     }
+    // }
+  }
 }
 </script>
+
+<style scoped>
+  .register-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  
+  .register-card {
+    width: 400px;
+    padding: 20px;
+  }
+  
+  .button-container {
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 20px;
+  }
+  </style>
+  
