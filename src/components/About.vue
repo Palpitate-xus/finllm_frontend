1f9b1e32e7fcce628842f152c8d2a0f822c5baee
Palpitate <xus1758011080@outlook.com>
2024-02-28 15:09:25 +0800

    update About page

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>
-      <el-row :gutter="20">
-        <h1>关于</h1>
-      </el-row>
+  <div class="about">
+    <h1 class="title">关于我们</h1>
+    <div class="author-info">
+      <!-- <img src="author-avatar.jpg" alt="Author Avatar" class="avatar"> -->
+      <div class="info">
+        <h2>作者：xus</h2>
+        <p>Email：palpitate.xus@mail.ecust.edu.cn</p>
+        <p>个人网站：<a href="https://www.example.com">https://www.example.com</a></p>
+      </div>
+    </div>
+    <!-- <div class="description">
+      <p>这是一个关于我们页面的示例，用于展示作者的信息。</p>
+      <p>您可以在此处添加更多关于作者的描述。</p>
+    </div> -->
   </div>
 </template>
 
@@ -19,5 +29,32 @@ export default {
 </script>
 
 <style scoped>
+.about {
+  padding: 20px;
+}
 
+.title {
+  font-size: 24px;
+  margin-bottom: 20px;
+}
+
+.author-info {
+  display: flex;
+  align-items: center;
+}
+
+.avatar {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  margin-right: 20px;
+}
+
+.info {
+  flex: 1;
+}
+
+.description {
+  margin-top: 20px;
+}
 </style>
