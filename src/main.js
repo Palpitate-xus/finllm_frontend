f022797a286b3d18c664ee1e6ab547257387129d
Palpitate <xus1758011080@outlook.com>
2024-02-29 14:46:48 +0800

    update route guard

--- src/main.js
+++ src/main.js
@@ -1,21 +1,43 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui';
+import ElementUI, { Notification } from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import { jwtDecode } from 'jwt-decode';
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI);
 
 router.beforeEach((to, from, next) => {
-  const isLoggedIn = localStorage.getItem('token'); // 假设 token 存在本地存储中
+  const token = localStorage.getItem('token'); // token 存在本地存储中
+  let isLoggedIn = false;
+  if (token) {
+    // 解码 token 获取过期时间
+    const decodedToken = jwtDecode(token);
+    const expirationTime = decodedToken.exp;
+
+    // 获取当前时间戳
+    const currentTime = Math.floor(Date.now() / 1000);
+    console.log(currentTime, expirationTime);
+    // 如果 token 已过期，则跳转到登录页
+    if (expirationTime > currentTime) {
+      isLoggedIn = true;
+    }
+  }
+
   if (to.meta.requiresAuth && !isLoggedIn) {
-    // 如果访问的页面需要登录权限且用户未登录，则跳转到登录页
+    // 如果访问的页面需要登录权限且用户未登录，则提示并跳转到登录页
+    Notification({
+      title: '提示',
+      message: '您未登录或登陆状态已过期，请先登录',
+      type: 'warning'
+    })
     next('/login');
   } else {
     next();
   }
+  
 });
 
 new Vue({
