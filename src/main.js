885a81d608c33db67d2c53a1fccbb946567848fe
Palpitate <xus1758011080@outlook.com>
2024-02-29 14:38:57 +0800

    add router guard

--- src/main.js
+++ src/main.js
@@ -8,12 +8,17 @@ Vue.config.productionTip = false
 
 Vue.use(ElementUI);
 
+router.beforeEach((to, from, next) => {
+  const isLoggedIn = localStorage.getItem('token'); // 假设 token 存在本地存储中
+  if (to.meta.requiresAuth && !isLoggedIn) {
+    // 如果访问的页面需要登录权限且用户未登录，则跳转到登录页
+    next('/login');
+  } else {
+    next();
+  }
+});
+
 new Vue({
   router,
   render: h => h(App),
 }).$mount('#app')
-
-// let myvue = new Vue({
-//   router,
-//   render: h => h(App),
-// }).$mount('#app')
