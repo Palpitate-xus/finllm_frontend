b65aa05446916e67aea9fa2d6b348141c87685c2
Palpitate <xus1758011080@outlook.com>
2024-03-18 21:18:06 +0800

    初步完成用户管理

--- src/components/UserManagement.vue
+++ src/components/UserManagement.vue
@@ -3,11 +3,16 @@
     <el-table :data="userData" stripe style="width: 100%">
       <el-table-column prop="id" label="ID" width="80"></el-table-column>
       <el-table-column prop="username" label="用户名"></el-table-column>
-      <el-table-column prop="user_email" label="邮箱"></el-table-column>
+      <el-table-column prop="email" label="邮箱"></el-table-column>
       <el-table-column prop="role" label="角色"></el-table-column>
+      <el-table-column prop="disabled" label="状态" width="124">
+        <template slot-scope="{ row }">
+          <el-tag>{{ row.disabled ? '已禁用' : '正常' }}</el-tag>
+        </template>
+      </el-table-column>
       <el-table-column prop="timestamp" label="操作" width="180">
         <template slot-scope="{ row }">
-          <span>{{ formatTimestamp(row.timestamp) }}</span>
+          <el-button @click="deleteUser(row)" type="danger" size="small">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -20,10 +25,11 @@ export default {
   name: 'UserManagement',
   data() {
     return {
-      userData: [
-        { id: 1, username: 'palpitate', user_email: 'palpitate@palpitate.com', role: 'admin' , timestamp: '2024-02-19 10:00:00' },
-      ]
+      userData: []
     };
+  },
+  mounted() {
+    this.fetchUsers();
   },
   methods: {
     formatTimestamp(timestamp) {
@@ -34,12 +40,27 @@ export default {
       await axiosInstance.get('/users/get_user_list')
         .then((response) => {
           console.log(response);
-          this.historyData = response.data.users;
+          this.userData = response.users;
         })
         .catch((error) => {
           console.log(error);
         });
-    }
+    },
+    async deleteUser(row) {
+      await axiosInstance.post('/users/delete_user', { params: { id: row.id } })
+        .then((response) => {
+          console.log(response);
+          this.$notify({
+            title: '提示',
+            message: '删除成功',
+            type: 'success'
+          });
+          this.fetchUsers();
+        })
+        .catch((error) => {
+          console.log(error);
+      })
+    },
   }
 }
 </script>
