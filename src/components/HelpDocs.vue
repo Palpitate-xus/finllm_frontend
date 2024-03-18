64846dfe0ec5ea987f90bd30a6aa24a9bab1202c
Palpitate <xus1758011080@outlook.com>
2024-03-18 22:29:56 +0800

    添加帮助页面

---
+++ src/components/HelpDocs.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app-container">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelpDocs',
+  data () {
+    return {
+      activeIndex: '/',
+      authorization: localStorage.getItem('role')=="admin"
+    }
+  },
+  watch: {
+    $route(to,from){
+      console.log(from.path);  // 从哪来
+      console.log(to.path);  // 到哪去
+      this.activeIndex = this.$route.path
+  }
+}
+}
+</script>
+
+<style></style>
+
