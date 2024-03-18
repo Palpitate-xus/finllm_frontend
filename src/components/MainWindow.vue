4c809053fa8916c2fb8f2530a1116bba1e45b4f7
Palpitate <xus1758011080@outlook.com>
2024-03-18 22:28:04 +0800

    add api to some functions

--- src/components/MainWindow.vue
+++ src/components/MainWindow.vue
@@ -28,9 +28,9 @@
           <el-button @click="handleClick2">财报分析</el-button>
           <el-button @click="handleClick3">文本总结</el-button>
           <el-button @click="handleClick4">市场预测</el-button>
-          <el-button @click="handleClick3">风险评估</el-button>
-          <el-button @click="handleClick3">事件分析</el-button>
-          <el-button @click="handleClick3">行业研究</el-button>
+          <el-button @click="handleClick5">风险评估</el-button>
+          <el-button @click="handleClick6">事件分析</el-button>
+          <el-button @click="handleClick7">行业研究</el-button>
         </el-col>
         <el-col :span="8">
           <el-input
@@ -96,10 +96,31 @@
           })
           .catch((error) => {
             console.log(error);
+            this.$notify({
+              title: '错误',
+              message: '情感分析失败，请检查输入内容',
+              type: 'error'
+            })
           });
       },
-      handleClick2() {
-        // 实现功能2
+      async handleClick2() {
+        await axiosInstance.post('/llms/analyze_finance', {
+          text: this.message,
+          name: "test",
+          description: "财报分析"
+        })
+          .then((response) => {
+            console.log(response);
+            this.output = response.finance_score;
+          })
+          .catch((error) => {
+            console.log(error);
+            this.$notify({
+              title: '错误',
+              message: '财报分析失败，请检查输入内容',
+              type: 'error'
+            })
+          });
       },
       async handleClick3() {
         await axiosInstance.post('/llms/summarize', {
@@ -113,6 +134,87 @@
           })
           .catch((error) => {
             console.log(error);
+            this.$notify({
+              title: '错误',
+              message: '文本总结失败，请检查输入内容',
+              type: 'error'
+            })
+          });
+      },
+      async handleClick4() {
+        await axiosInstance.post('/llms/forecast', {
+          text: this.message,
+          name: "test",
+          description: "市场预测"
+        })
+          .then((response) => {
+            console.log(response);
+            this.output = response.forecast_res;
+          })
+          .catch((error) => {
+            console.log(error);
+            this.$notify({
+              title: '错误',
+              message: '市场预测失败，请检查输入内容',
+              type: 'error'
+            })
+          });
+      },
+      async handleClick5() {
+        await axiosInstance.post('/llms/risk_assessment', {
+          text: this.message,
+          name: "test",
+          description: "风险评估"
+        })
+          .then((response) => {
+            console.log(response);
+            this.output = response.risk_score;
+          })
+          .catch((error) => {
+            console.log(error);
+            this.$notify({
+              title: '错误',
+              message: '风险评估失败，请检查输入内容',
+              type: 'error'
+            })
+          });
+      },
+      async handleClick6() {
+        await axiosInstance.post('/llms/event_analysis', {
+          text: this.message,
+          name: "test",
+          description: "事件分析"
+        })
+          .then((response) => {
+            console.log(response);
+            this.output = response.event_analysis_res;
+          })
+          .catch((error) => {
+            console.log(error);
+            this.$notify({
+              title: '错误',
+              message: '事件分析失败，请检查输入内容',
+              type: 'error'
+            })
+          });
+      },
+      async handleClick7() {
+        await axiosInstance.post('/llms/industry_research', {
+          text: this.message,
+          name: "test",
+          description: "行业研究"
+        })
+          .then((response) => {
+            console.log(response);
+            this.output = response.industry_res;
+          })
+          .catch((error) => {
+            console.log(error);
+            this.$notify({
+              title: '错误',
+              message: '行业研究失败，请检查输入内容',
+              type: 'error'
+            })
           });
       },
     },
